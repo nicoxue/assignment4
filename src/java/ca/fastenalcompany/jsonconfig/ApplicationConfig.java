29413b552060ed2766f6b458085ffe6fe2ff3918
JiaJun Xue <nicoxue0324@gmail.com>
2015-03-07 19:27:15 -0500

    step1

---
+++ src/java/ca/fastenalcompany/jsonconfig/ApplicationConfig.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.fastenalcompany.jsonconfig;
+
+/**
+ *
+ * @author JIAJUN XUE <nicoxue0324@gmail.com>
+ */
+@javax.ws.rs.ApplicationPath("")
+public class ApplicationConfig {
+}
